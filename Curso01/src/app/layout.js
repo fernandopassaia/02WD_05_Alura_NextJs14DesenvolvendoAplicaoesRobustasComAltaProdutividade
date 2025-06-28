419efa90759bbce970a443afe02f099390266c37
FernandoPassaia MacBook Pro M4 <fernandopassaia@futuradata.com.br>
2025-06-28 20:45:44 -0300

    Finishing 1 Class

--- Curso01/src/app/layout.js
+++ Curso01/src/app/layout.js
@@ -1,15 +1,20 @@
+import { Aside } from '@/components/Aside'
+import './globals.css'
 
 export const metadata = {
-  title: "Code Connect",
-  description: "Uma Rede Social para Desenvolvedores",
-};
+  title: 'Code Connect',
+  description: 'Uma rede social para devs!',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body>
-        {children}
+        <div className='app-container'>
+          <Aside />
+          {children}
+        </div>
       </body>
     </html>
-  );
+  )
 }
